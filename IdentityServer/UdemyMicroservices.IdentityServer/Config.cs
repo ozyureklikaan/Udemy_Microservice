cccfed785ceeb8b13af839e95891a9b5a65103a1
Kaan Ozyurekli <ozyureklikaan@gmail.com>
2021-04-26 01:26:21 +0300

    Develop fake payment api.

--- IdentityServer/UdemyMicroservices.IdentityServer/Config.cs
+++ IdentityServer/UdemyMicroservices.IdentityServer/Config.cs
@@ -19,6 +19,7 @@ namespace UdemyMicroservices.IdentityServer
                 new ApiResource("resource_basket"){ Scopes = { "basket_fullpermission" } },
                 new ApiResource("resource_discount"){ Scopes = { "discount_fullpermission" } },
                 new ApiResource("resource_order"){ Scopes = { "order_fullpermission" } },
+                new ApiResource("resource_fake_payment"){ Scopes = { "fake_payment_fullpermission" } },
                 new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
             };
 
@@ -39,6 +40,7 @@ namespace UdemyMicroservices.IdentityServer
                 new ApiScope("basket_fullpermission", "Basket API için tam erişim"),
                 new ApiScope("discount_fullpermission", "Discount API için tam erişim"),
                 new ApiScope("order_fullpermission", "Order API için tam erişim"),
+                new ApiScope("fake_payment_fullpermission", "Fake Payment API için tam erişim"),
                 new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
             };
 
@@ -70,6 +72,7 @@ namespace UdemyMicroservices.IdentityServer
                         "basket_fullpermission",
                         "discount_fullpermission",
                         "order_fullpermission",
+                        "fake_payment_fullpermission",
                         IdentityServerConstants.StandardScopes.Email,
                         IdentityServerConstants.StandardScopes.OpenId,
                         IdentityServerConstants.StandardScopes.Profile,
---
+++ Services/FakePayment/UdemyMicroservices.FakePayment/Controllers/FakePaymentsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UdemyMicroservices.Shared.ControllerBases;
+using UdemyMicroservices.Shared.Dtos;
+
+namespace UdemyMicroservices.FakePayment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FakePaymentsController : CustomBaseController
+    {
+        [HttpPost]
+        public IActionResult ReceivePayment()
+        {
+            return CreateActionResultInstance(Response<NoContent>.Success(200));
+        }
+    }
+}
