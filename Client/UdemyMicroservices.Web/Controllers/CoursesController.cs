b7e0bf53d4067b962c150b83e70df32e2e7fb4a4
Kaan Ozyurekli <ozyureklikaan@gmail.com>
2021-07-18 02:01:04 +0300

    Develop course page.

--- Client/UdemyMicroservices.Web/Controllers/CoursesController.cs
+++ Client/UdemyMicroservices.Web/Controllers/CoursesController.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using UdemyMicroservices.Shared.Services;
+using UdemyMicroservices.Web.Models.Catalog;
 using UdemyMicroservices.Web.Services.Interfaces;
 
 namespace UdemyMicroservices.Web.Controllers
@@ -24,6 +26,86 @@ namespace UdemyMicroservices.Web.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _catalogService.GetAllCourseByUserIdAsync(_sharedIdentityService.GetUserId));
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var categories = await _catalogService.GetAllCategoryAsync();
+
+            ViewBag.categoryList = new SelectList(categories, "Id", "Name");
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CourseCreateInput course)
+        {
+            var categories = await _catalogService.GetAllCategoryAsync();
+
+            ViewBag.categoryList = new SelectList(categories, "Id", "Name");
+
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            course.UserId = _sharedIdentityService.GetUserId;
+
+            await _catalogService.CreateCourseAsync(course);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Update(string id)
+        {
+            var categories = await _catalogService.GetAllCategoryAsync();
+
+            var course = await _catalogService.GetCourseByCourseId(id);
+
+            if (course == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewBag.categoryList = new SelectList(categories, "Id", "Name", course.Id);
+
+            CourseUpdateInput courseUpdateInput = new CourseUpdateInput()
+            {
+                Id = course.Id,
+                Name = course.Name,
+                Description = course.Description,
+                Price = course.Price,
+                Feature = course.Feature,
+                CategoryId = course.CategoryId,
+                UserId = course.UserId,
+                Picture = course.Picture
+            };
+
+            return View(courseUpdateInput);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(CourseUpdateInput course)
+        {
+            var categories = await _catalogService.GetAllCategoryAsync();
+
+            ViewBag.categoryList = new SelectList(categories, "Id", "Name", course.Id);
+
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            await _catalogService.UpdateCourseAsync(course);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            await _catalogService.DeleteCourseAsync(id);
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- Client/UdemyMicroservices.Web/Models/Catalog/CourseCreateInput.cs
+++ Client/UdemyMicroservices.Web/Models/Catalog/CourseCreateInput.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +8,27 @@ namespace UdemyMicroservices.Web.Models.Catalog
 {
     public class CourseCreateInput
     {
+        [Display(Name="Kurs ismi")]
+        [Required]
         public string Name { get; set; }
+        
+        [Display(Name = "Kurs açıklaması")]
+        [Required]
         public string Description { get; set; }
+        
+        [Display(Name = "Kurs fiyatı")]
+        [Required]
         public decimal Price { get; set; }
+        
         public string UserId { get; set; }
+        
+        [Display(Name = "Kurs resmi")]
         public string Picture { get; set; }
+        
         public FeatureViewModel Feature { get; set; }
+
+        [Display(Name = "Kurs kategorisi")]
+        [Required]
         public string CategoryId { get; set; }
     }
 }
