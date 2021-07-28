c6d8b1ad4bd26dada28a4755044a9c9a43f71cbb
Kaan Ozyurekli <ozyureklikaan@gmail.com>
2021-07-29 01:30:12 +0300

    Develop docker-compose.

--- Services/Discount/UdemyMicroservice.Discount/Program.cs
+++ Services/Discount/UdemyMicroservice.Discount/Program.cs
@@ -1,7 +1,10 @@
+using Dapper;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Npgsql;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +16,20 @@ namespace UdemyMicroservice.Discount
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+
+                var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+
+                var _dbConnection = new NpgsqlConnection(configuration.GetConnectionString("PostgreSql"));
+
+                _dbConnection.Execute("create table Discount(Id serial primary key, UserId varchar(200) not null, Rate smallint not null, Code varchar(50) not null, CreatedDate timestamp not null default CURRENT_TIMESTAMP)");
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- Services/Order/UdemyMicroservices.Order.API/Program.cs
+++ Services/Order/UdemyMicroservices.Order.API/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UdemyMicroservices.Order.Infrastructure;
 
 namespace UdemyMicroservices.Order.API
 {
@@ -13,7 +16,18 @@ namespace UdemyMicroservices.Order.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+
+                var orderDbContext = serviceProvider.GetRequiredService<OrderDbContext>();
+
+                orderDbContext.Database.Migrate();
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
